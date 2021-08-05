0d32ebae841a75b6ddbfb42d77cb0a27f49e2669
AyazSung <88406440+AyazSung@users.noreply.github.com>
2021-08-06 02:50:31 +0300

    Add files via upload

---
+++ LAB_!.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+int main()
+{
+    // SQUARE EQUATION---------------------------
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    int Dis = b * b - 4 * a * c;
+
+    if (Dis < 0) cout << "No SOLUTION";
+    else
+    {
+        double r1 = (-b + sqrt(Dis)) / (2 * a);
+        double r2 = (-b - sqrt(Dis)) / (2 * a);
+
+        cout << r1 << "*****" << r2;
+    }
+
+}
